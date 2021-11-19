c466a4a650f9cd31f9e98c9a8fedb206181ac2c7
cnicuta <cnicuta@fusionworks.md>
2021-11-19 12:03:09 +0200

    init commit

---
+++ src/main/java/com/camel/bootcamel/route/SimpleRouteBuilder.java
@@ -0,0 +1,27 @@
+package com.camel.bootcamel.route;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SimpleRouteBuilder extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+        from("file:source-folder?noop=true")
+                .filter(header(Exchange.FILE_NAME).startsWith("forMoving"))
+                    .choice()
+                        .when(body().contains("rock"))
+                            .log("moving a rock")
+                        .when(body().contains("paper"))
+                            .log("moving a paper")
+                        .otherwise()
+                            .log("moving a regular one")
+                            .to("direct:regular-processor")
+                    .end()
+                .to("file:destination-folder");
+
+        from("direct:regular-processor")
+                .log("dat shit is crazy");
+    }
+}
---
+++ src/test/java/com/camel/bootcamel/BootCamelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camel.bootcamel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootCamelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
